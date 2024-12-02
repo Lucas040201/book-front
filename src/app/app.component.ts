67f17c780b9d6a376f89c0c576da6ec227c8a522
Lucas Mendes <lucasdasilvamendes123456@hotmail.com>
2024-12-02 01:29:19 -0300

    chore: adjusts angular config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+  ],
+  standalone: true,
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
-  title = 'book-front';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import {CommomPageComponent} from './shared/components/commom-page/commom-page.component';
+import {BookComponent} from './pages/book/book.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CommomPageComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+      },
+      {
+        path: 'book',
+        component: BookComponent,
+        children: [
+          {
+            path: 'create',
+            loadComponent: () => import('./pages/book/create/create.component').then(m => m.CreateComponent),
+          },
+          {
+            path: 'edit/:id',
+            loadComponent: () => import('./pages/book/update/update.component').then(m => m.UpdateComponent),
+          },
+          {
+            path: ':id',
+            loadComponent: () => import('./pages/book/show/show.component').then(m => m.ShowComponent),
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
